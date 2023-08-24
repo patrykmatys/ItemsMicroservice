238b44441af8aef163508146af16d9b6780b877d
Patryk Matys <patrykmatys0@gmail.com>
2023-08-24 19:56:16 +0200

    prepare for gateway

--- src/main/java/org/thesis/Main.java
+++ src/main/java/org/thesis/Main.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test");
         SpringApplication.run(Main.class, args);
     }
 }
--- src/main/java/org/thesis/controllers/ItemController.java
+++ src/main/java/org/thesis/controllers/ItemController.java
@@ -3,6 +3,7 @@ package org.thesis.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.thesis.models.Item;
 import org.thesis.services.ItemService;
@@ -10,21 +11,22 @@ import org.thesis.services.ItemService;
 import java.util.UUID;
 
 @RestController
+@RequestMapping("/api/v1/items")
 public class ItemController {
     @Autowired
     private ItemService itemService;
 
-    @GetMapping("/api/v1/items/{id}")
+    @GetMapping("/{id}")
     public Item getItemById(@PathVariable UUID id) {
         return itemService.getItemById(id).orElse(null);
     }
 
-    @GetMapping("/api/v1/items")
+    @GetMapping()
     public Iterable<Item> getItems() {
         return itemService.getItems();
     }
 
-    @GetMapping("/api/v1/items/category/{id}")
+    @GetMapping("/category/{id}")
     public Iterable<Item> getItemsByCategory(@PathVariable UUID id) {
         return itemService.getItemsByCategoryId(id);
     }
